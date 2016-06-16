0281b8be93148c23ef3739d144ce51c9826f1855
hsusmita <susmita.horrow@gmail.com>
2016-06-16 11:04:34 +0530

    handle show/hide of as per selection

--- js/menu.js
+++ js/menu.js
@@ -3,32 +3,13 @@ jQuery.noConflict();
 jQuery(function($) {
  $("#menu li").click(function() 
     { 
-    	var index = $( "#menu li" ).index( this );
-    			console.log(index);
-
-  	 	switch(index) {
-    		case 0:
-    			console.log(index);
-    			$('#project').hide()
-    			$('#home').show()
-    			break;
-    		case 1: 
-    			console.log(index);
-    			// $("#project").addClass("enable");
-    			// $("#home").addClass("disable");
-    			// $('#project').css('opacity', '1');
-    			// $("#home").css('opacity', '0');
-    			$('#project').show()
-    			$('#home').hide()
-
-    			break;
-    		case 2:
-    			console.log(index);
-    			break;
-    		case 3:
-    		    console.log(index);
-    		    break;
-
-    	}
+    	var currentIndex = $( "#menu li" ).index( this );
+         $( "#mainContent .row" ).each(function( index ) {
+            if (index == currentIndex) {
+                $(this).show()
+            } else {
+                $(this).hide()
+            }
+        });
     });
 });
